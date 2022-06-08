d757bc9e65feb49b10bdfb558de25a8b3e4457b4
Charlie <harrischarliep@gmail.com>
2022-06-08 18:42:39 -0400

    cleaned up comments and logging

--- routes/api/puzzle.js
+++ routes/api/puzzle.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-// const solve = require('../../src/solve.js');
 const solve = require('../../src/better-solve.js');
 
 router.get('/test', (req, res) => res.send('solve test'));
@@ -11,19 +10,3 @@ router.post('/', (req, res) => {
 });
 
 module.exports = router;
-
-// {
-//     "puzzle": [
-//     [null,  null,   4,          null,   null,   9,          6,      5,      null],
-//     [8,     5,      7,          6,      null,   null,       null,   null,   4],
-//     [null,  3,      null,       null,   null,   null,       8,      7,      1],
-
-//     [null,  null,   8,          null,   null,   null,       null,   null,   null],
-//     [null,  6,      9,          4,      7,      8,          1,      3,      null],
-//     [null,  null,   null,       null,   null,   null,       4,      null,   null],
-
-//     [2,     9,      5,          null,   null,   null,       null,   4,      null],
-//     [7,     null,   null,       null,   null,   5,          9,      1,      6],
-//     [null,  8,      1,          3,      null,   null,       5,      null,   null]
-//     ]
-// }
--- src/better-solve.js
+++ src/better-solve.js
@@ -57,31 +57,18 @@ const rowToSquareCoords = (rowR, rowC) => {
 }
 
 const initPossibleVals = rows => {
-    // console.log('************************** InitPossibleValues **************************');
-    // console.log(rows);
-
     const allPossibleVals = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     const possibleVals = [];
     let remaining = puzzleSize ** 2;
     for (let r = 0; r < puzzleSize; r++) {
         const row = [];
         for (let c = 0; c < puzzleSize; c++) {
-            const col = [];
             const val = rows[r][c];
-            if (val) {
-                col.push([val]);
-                remaining--;
-            } else {
-                col.push(...allPossibleVals);
-            }
-            console.log(`col length: ${col.length}`);
+            const col = val ? remaining-- && [val] : [...allPossibleVals];
             row.push(col);
         }
         possibleVals.push(row);
     }
-    
-    // logPossible(possibleVals);
-    // console.log('************************** InitPossibleValues **************************');
 
     return [possibleVals, remaining];
 }
@@ -95,12 +82,6 @@ const solve = puzzle => {
     let solved = false;
     let iteration = 0;
     while (!solved && iteration < maxIterations) {
-        console.log(`**************** ITERATION ${iteration + 1} ****************`);
-        // prettyPrint(rows, "rows");
-        // prettyPrint(cols, "cols");
-        // prettyPrint(squares, "squares");
-        // logPossible(possibleVals);
-
         for (let r = 0; r < puzzleSize; r++) {
             for (let c = 0; c < puzzleSize; c++) {
                 if (possibleVals[r][c].length === 1) {
@@ -116,23 +97,13 @@ const solve = puzzle => {
                 const [squareR, squareC] = rowToSquareCoords(rowR, rowC);
                 const inSquare = [...squares[squareR]].filter(e => e);
 
-                // console.log(`inRow: ${inRow}`);
-                // console.log(`inCol${inCol}`);
-                // console.log(`inSquare: ${inSquare}`);
-    
-                console.log(`BEFORE possibleVals[${r}][${c}]=${possibleVals[r][c]}`);
                 possibleVals[r][c] = [...possibleVals[r][c]].filter(e => !inRow.includes(e) && !inCol.includes(e) && !inSquare.includes(e));
-                console.log(`AFTER possibleVals[${r}][${c}]=${possibleVals[r][c]}`);
                 if (possibleVals[r][c].length == 1) {
                     const val = possibleVals[r][c][0];
-                    // console.log(`updating val: ${val}`);
-                    // console.log(`BEFORE rows: ${rows[rowR][rowC]}, cols: ${cols[colR][colC]}, squares: ${squares[squareR][squareC]}, remaining: ${remaining}`);
                     rows[rowR][rowC] = val;
                     cols[colR][colC] = val;
                     squares[squareR][squareC] = val;
                     remaining--;
-                    // console.log(`AFTER rows: ${rows[rowR][rowC]}, cols: ${cols[colR][colC]}, squares: ${squares[squareR][squareC]}, remaining: ${remaining}`);
-                    console.log(`Resolved (${r}, ${c}) => ${val}, iteration #${iteration}, ${remaining} remaining`);
                 }
             }
         }
@@ -140,10 +111,6 @@ const solve = puzzle => {
             solved = true;
         }
         iteration++;
-
-        console.log(`***********************************************************\n\n\n`);
-
-        // prettyPrint(rows);
     }
 
     return {
@@ -154,98 +121,4 @@ const solve = puzzle => {
     };
 }
 
-
-// TODO: remove logging
-const logPossible = possibleVals => {
-    console.log("Possible values: ");
-    for (let r = 0; r < puzzleSize; r++) {
-        for (let c = 0; c < puzzleSize; c++) {
-            const vals = possibleVals[r][c];
-            console.log(`\t (${r}, ${c}) => ${vals}`);
-        }
-    }
-}
-
-const prettyPrint = (arr, tag) => {
-    console.log(tag);
-    arr.forEach(e => console.log(`\t ${e}`));
-    // logMarker();
-    // for (let r = 0; r < puzzleSize; r++) {
-    //     let line;
-    //     for (let c = 0; c < puzzleSize; c++) {
-    //         if (c % squareSize == 0) {
-    //             line += '\t';
-    //         }
-    //         line += rows[r][c] + ' ';
-    //     }
-    //     if (r % squareSize == 0) {
-    //         console.log('\n');
-    //         console.log(line);
-    //     }
-    // }
-    // logMarker();
-}
-
-const logMarker = () => console.log('**********************************************************');
-//
-
-
 module.exports = solve;
-
-
-/*
-row: 
-[
-    [(r0c0, r0c1, r0c2)     (r0c3 r0c4 r0c5)    (roc6 r0c7 r0c8)],
-    [(r1c0, r1c1, r1c2)     (r1c3 r1c4 r1c5)    (r1c6 r1c7 r1c8)],
-    [(r2c0, r2c1, r2c2)     (r2c3 r2c4 r2c5)    (r2c6 r2c7 r2c8)],
-
-    [(r3c0, r3c1, r3c2)     (r3c3 r3c4 r3c5)    (r3c6 r3c7 r3c8)],
-    [(r4c0, r4c1, r4c2)     (r4c3 r4c4 r4c5)    (r4c6 r4c7 r4c8)],
-    [(r5c0, r5c1, r5c2)     (r5c3 r5c4 r5c5)    (r5c6 r5c7 r5c8)],
-
-    [(r6c0, r6c1, r6c2)     (r6c3 r6c4 r6c5)    (r6c6 r6c7 r6c8)],
-    [(r7c0, r7c1, r7c2)     (r7c3 r7c4 r7c5)    (r7c6 r7c7 r7c8)],
-    [(r8c0, r8c1, r8c2)     (r8c3 r8c4 r0c5)    (r8c6 r8c7 r8c8)],
-]
-*/
-
-/*
-
-square:
-[
-    [r0c0, r0c1, r0c2, r1c0, r1c1, r1c2, r2c0, r2c1, r2c2],
-    ...
-]
-
-*/
-
-    // const updateRows = (rowR, rowC, val) => {
-    //     rows[rowR][rowC] = val;
-        
-    //     const [colR, colC] = rowToColCoords(rowR, rowC);
-    //     cols[colR][colC] = val;
-
-    //     const [squareR, squareC] = rowToSquareCoords(rowR, rowC);
-    //     squares[squareR][squareC] = val;
-    // }
-
-    // const updateCols = (colR, colC, val) => {
-    //     cols[colR][colC] = val;
-
-    //     const [rowR, rowC] = colToRowCoords(colR, colC);
-    //     rows[rowR][rowC] = val;
-
-    //     const [squareR, squareC] = rowToSquareCoords(rowR, rowC);
-    //     squares[squareR][squareC] = val;
-    // }
-
-    // const updateSquares = (squareR, squareC, val) => {
-    //     squares[squareR][squareC] = val;
-
-    //     const [rowR, rowC] = squareToRowCoords(squareR, squareC);
-    //     rows[rowR][rowC] = val;
-
-    //     const [colR, colC] = rowToColCoords(rowR, rowC);
-    //     cols[colR][colC] = val;
-    // }
